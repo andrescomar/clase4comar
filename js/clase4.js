d5bf5f1a4332d72b402b31f89fe2e091b7f25706
andrescomar <andrescomar@hotmail.com>
2021-03-29 00:48:13 -0300

    ultimos cambios

--- js/clase4.js
+++ js/clase4.js
@@ -32,3 +32,23 @@ alert(caluladora(numero1,numero2, operacion));
 for (let index = 0; index < 20; index++) {
     saludar();  
 }
+
+const suma = (x,y) => x + y ;
+const resta = (x,y) => x - y ;
+const iva = x => x * 0.21;
+
+// Version function
+function iva2 ( x ){
+    return x * 0.21;
+}
+
+alert(suma(2,3));
+
+let precioProducto = 5000;
+let precioDescuento = 500;
+
+let precioMasIva = suma(precioProducto, iva(precioProducto));
+
+let precioFinal = resta(precioMasIva, precioDescuento);
+
+alert(precioFinal); 
